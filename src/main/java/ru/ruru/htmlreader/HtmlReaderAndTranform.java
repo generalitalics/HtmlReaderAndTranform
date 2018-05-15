648cff5c588f69986b76847dcdbf32eab9553b49
generalitalics <generalitalics@mail.ru>
2018-05-15 12:23:01 +0300

    Добавил 2 стартовых класса

---
+++ src/main/java/ru/ruru/htmlreader/HtmlReaderAndTranform.java
@@ -0,0 +1,12 @@
+package ru.ruru.htmlreader;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HtmlReaderAndTranform {
+
+//https://cnclip.net/video/-l8Ji17d65Y/%D0%BF%D0%B0%D1%80%D1%81%D0%B8%D0%BD%D0%B3-html-%D0%B2-java.html
+    public static void main(String[] args) {
+        List<Article> articleList = new ArrayList<Article>();
+    }
+}
